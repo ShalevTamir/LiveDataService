ffb4e9b38254e3a7ae606446a643b10aaddb090d
Shalev Tamir <shalevtamir1009@gmail.com>
2024-01-11 07:54:51 +0200

    Add project files.

---
+++ LiveDataService/LiveParameters/Controllers/LiveParametersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LiveDataService.LiveParameters.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LiveParametersController : Controller
+    {
+        [HttpGet()]
+        public async Task GetParameters()
+        {
+            if(HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+                string message = "This is a message from the socket";
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                var arraySegment = new ArraySegment<byte>(messageBytes, 0, messageBytes.Length);
+                await webSocket.SendAsync(
+                    arraySegment,
+                    System.Net.WebSockets.WebSocketMessageType.Text,
+                    true,
+                    CancellationToken.None
+                    );
+
+
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+    }
+}
